b3cc1b89e6dd05ea8b831fe8eac210e37b09c360
SebastiaanYN <sebastiaanyn@gmail.com>
2020-03-02 16:50:00 +0100

    build(webpack): improve build configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,41 @@
 /* eslint-disable @typescript-eslint/no-var-requires, @typescript-eslint/no-require-imports */
 const path = require('path');
 
-module.exports = (_, argv) => ({
-  entry: './src/index',
-  mode: 'development',
-  devtool: 'eval',
-  module: {
-    rules: [
-      {
-        test: /\.ts$/,
-        loader: 'ts-loader',
-      },
+module.exports = (_, argv) => {
+  const prod = argv.mode === 'production';
+
+  return {
+    entry: './src/index',
+    mode: 'development',
+    devtool: prod ? false : 'eval',
+
+    output: {
+      filename: '[name].js',
+      chunkFilename: '[name].[contenthash].js',
+
+      library: 'webBeautify',
+      libraryTarget: 'umd',
+      globalObject: 'this',
+
+      publicPath: prod ? '' : '../dist/',
+      path: path.resolve(__dirname, 'dist'),
+    },
+
+    module: {
+      rules: [
+        {
+          test: /\.ts$/,
+          loader: 'ts-loader',
+        },
+      ],
+    },
+
+    resolve: {
+      extensions: ['.ts', '.js'],
+    },
+
+    externals: [
+      '@microsoft/typescript-etw', // hide warning about missing optional dependency
     ],
-  },
-  resolve: {
-    extensions: [
-      '.ts',
-      '.js',
-    ],
-  },
-  output: {
-    filename: '[name].js',
-    chunkFilename: '[contenthash].js',
-    publicPath: argv.mode === 'production' ? '' : '../dist/',
-    libraryTarget: 'umd',
-    path: path.resolve(__dirname, 'dist'),
-  },
-});
+  };
+};
